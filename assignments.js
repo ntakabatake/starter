25e0ad5adb136aeb38b4ab45b046f6570802410a
Nathalia Takabatake <nathaliatakabatake@gmail.com>
2022-11-21 14:48:32 -0500

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,24 @@
+const country = "United States";
+const continent = "North America";
+let population = 3231.9;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "English";
+const country = "United States";
+const continent = "North America";
+const isIsland = false;
+
+//isIsland = true;
+gh
